6f2c92ec08b5753a5db895bafc182798fc550277
pujith22 <pujith22.sde@gmail.com>
2025-11-07 00:02:29 +0530

    my first http server

--- server.cpp
+++ server.cpp
@@ -6,6 +6,14 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <httplib.h>
+
+using httplib::Request;
+
+using httplib::Response;
+
+using std::cout;
+
 void error(char *msg)
 {
     perror(msg);
@@ -14,59 +22,22 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n1, n2;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
+      httplib::Server server;
 
-     /* create socket */
+    server.Get("/", [](const Request& req, Response& res) {
+      cout<<"Get request received at / endpoint!!"<<"\n";
+    res.set_content("Hello World!", "text/plain");
+  });
 
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
+  server.Get("/stop", [&](const Request& req, Response& res) {
+    cout<<"Get request received at /stop endpoint"<<"\n";
+    server.stop();
+  });
 
-     /* fill in port number to listen on. IP address can be anything (INADDR_ANY) */
+  cout<<"Http server started and about to listen at localhost:2222 !!";
 
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
+  server.listen("localhost", 2222);
 
-     /* bind socket to this port number on this machine */
 
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     
-     /* listen for incoming connection requests */
-
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-
-     /* accept a new request, create a newsockfd */
-
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-
-     /* read message from client */
-
-     do {
-       bzero(buffer,256);
-       n1 = read(newsockfd,buffer,255);
-       if (n1 < 0) error("ERROR reading from socket");
-       printf("Here is the message: %s\n",buffer);
-
-       /* send reply to client */
-       
-       n2 = write(newsockfd,"I got your message",18);
-       if (n2 < 0) error("ERROR writing to socket");
-     } while (n1 > 0 && n2 > 0);
-       
        return 0; 
 }
